91055e2431b25bff8301c6047e88c2501eaa35e8
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2022-07-07 20:30:04 -0700

    header/imp split on rng

--- src/tools/rng.hpp
+++ src/tools/rng.hpp
@@ -12,40 +12,21 @@ class prng {
     
 public:
 
-    prng () :
-    seed(std::random_device{}()), engine(std::mt19937 {seed}) {}
-    prng (std::mt19937::result_type seed) : 
-    seed(seed), engine(std::mt19937 {seed}) {}
+    prng ();
+    prng (std::mt19937::result_type seed);
 
     // Same device and seed, but 'restarted'
     // Default copy keeps progress
-    prng copy () {
-        return prng(seed);
-    }
+    prng copy ();
 
-    std::mt19937::result_type get_seed () {
-        return seed;
-    }
+    std::mt19937::result_type get_seed ();
 
-    double uniform () {
-        return uniform_(engine);
-    }
+    double uniform ();
 
-    int random_int (int n) {
-        return int (this->uniform() * n);
-    }
+    int random_int (int n);
 
     // samples an index from a probability distribution
-    int sample_pdf (double* input, int k) {
-        double p = this->uniform();
-        for (int i = 0; i < k; ++i) {
-            p -= input[i];
-            if (p <= 0) {
-                return i;
-            }
-        }
-        return 0;
-    }
+    int sample_pdf (double* input, int k);
 
     template <typename T, int size>
     int sample_pdf (std::array<T, size> input, int k) {
