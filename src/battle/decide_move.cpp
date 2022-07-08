8852e65cf7b9e88a3fccc6766f0f48a2a8a05fdb
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2022-07-07 20:29:49 -0700

    remove stuff

--- src/battle/decide_move.cpp
+++ src/battle/decide_move.cpp
@@ -4,5 +4,5 @@
 // place to implement decision making
 void Battle::decide_move(Team &team){
     team.prev_movechoice = team.movechoice;
-    team.movechoice = team.move_options[get_random(0, static_cast<int>(team.move_options.size()) - 1)];
+    team.movechoice = team.move_options[this->transition.random(static_cast<int>(team.move_options.size()))];
 }
