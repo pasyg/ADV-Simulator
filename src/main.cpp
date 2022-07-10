06f8755e8b2e21ac49ac6f02902d45a5cc778add
pasyg <pgekle@gmail.com>
2022-07-10 23:54:35 +0200

    w/e

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
-#pragma once
-
 #include "test.hpp"
 
-constexpr int played_games = 10;
+constexpr int played_games = 5;
 
 std::string create_filepath(){
     auto time = std::chrono::steady_clock::now();
@@ -14,45 +12,52 @@ std::string create_filepath(){
 
 int main(int argc, char* argv[]) {
 
-  // Test all_test;
-  // all_test.test_all();
+  Test all_test;
+  all_test.test_all();
 
   int w1 = 0, w2 = 0, tie = 0;
 
-  std::cout << "Start gaming!\n";
-
+  std::cout << "\nStart gaming!\n";
+  
   std::vector<Team> teams = import_team("brr");
   {
-    Timer timer;
-    for(int i = 0; i < played_games; ++i){
-      if(i == played_games / 10){ std::cout << "10%\n"; }
-      if(i == played_games / 5){ std::cout << "20%\n"; }
-      if(i == played_games / 2){ std::cout << "50%\n"; }
+  //   Timer timer;
+  //   int fract = played_games / 10;
+  //   for(int i = 0; i < played_games; ++i){
+  //     if(i == played_games){ std::cout << "10%\n"; }
+  //     if(i == played_games * 2){ std::cout << "20%\n"; }
+  //     if(i == played_games * 3){ std::cout << "30%\n"; }
+  //     if(i == played_games * 4){ std::cout << "40%\n"; }
+  //     if(i == played_games * 5){ std::cout << "50%\n"; }
+  //     if(i == played_games * 6){ std::cout << "60%\n"; }
+  //     if(i == played_games * 7){ std::cout << "70%\n"; }
+  //     if(i == played_games * 8){ std::cout << "80%\n"; }
+  //     if(i == played_games * 9){ std::cout << "90%\n"; }
 
-      Battle battle(teams[5], teams[23], create_filepath());
+  //     Battle battle(teams[5], teams[23]);
 
-      int winner = battle.play_battle();
-      // log stuff in battle.cpp
-      switch(winner){
-        case 0: 
-          ++w1;
-          battle.logger << "|win|Player 1";
-          break;
-        case 1:
-          ++w2;
-          battle.logger << "|win|Player 2";
-          break;
-        case 2:
-          ++tie;
-          battle.logger << "|tie|";
-          break;
-      }
-    }
-  std::cout << "Turn cap: " << constants::turn_cap << "\n";
-  std::cout << played_games << " games played in:\n";
-  std::cout << "\nTeam 1 wins: " << w1;
-  std::cout << "\nTeam 2 wins: " << w2;
-  std::cout << "\nTies: " << tie << "\n";
+  //     int winner = battle.play_battle();
+  //     // log stuff in battle.cpp
+  //     switch(winner){
+  //       case 0: 
+  //         ++w1;
+  //         //battle.logger << "|win|Player 1";
+  //         break;
+  //       case 1:
+  //         ++w2;
+  //         //battle.logger << "|win|Player 2";
+  //         break;
+  //       case 2:
+  //         ++tie;
+  //         //battle.logger << "|tie|";
+  //         break;
+  //     }
+  //   }
+  // std::cout << "Turn cap: " << constants::turn_cap << "\n";
+  // std::cout << "\nTeam 1 wins: " << w1;
+  // std::cout << "\nTeam 2 wins: " << w2;
+  // std::cout << "\nTies: " << tie << "\n";
+  // std::cout << played_games << " games played in:\n";
 
   return 0;
   } 
